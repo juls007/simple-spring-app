d63449ff41f47b524841dc28c7699519a29bdf59
Julius Cardozo <juls007@outlook.com>
2021-03-21 00:10:27 -0400

    updated string returned by controller

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure App Service (in the staging slot)!";
+        LOGGER.info("Calling home api end point");
+        return "Hello from Azure App Service!";
     }
 }
